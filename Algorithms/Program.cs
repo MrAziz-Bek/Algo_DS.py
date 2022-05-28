56370c339f1fe8d7e3b8c3a68ddb95e59d5992c7
BobAzizbek <boboyevazizbek2006@gmail.com>
2022-05-28 22:11:49 +0500

    Algorithms

---
+++ Algorithms/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine(findMaximum(23, 34, 12));
+
+static int findMaximum(int a, int b, int c)
+{
+    int max = a;
+
+    if (max < b)
+        max = b;
+    if (max < c)
+        max = c;
+
+    return max;
+}
